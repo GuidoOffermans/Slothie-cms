4de82a9ccf1d716060e2248a125bfa9ff43c1385
guido offermans <guidooffermans@hotmail.com>
2022-01-13 22:33:32 +0100

    add supabase

---
+++ utils/supabaseClient.ts
@@ -0,0 +1,8 @@
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { SupabaseClientOptions } from '@supabase/supabase-js/dist/main/lib/types';
+
+const supabaseUrl: string = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey: string = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supaOptions: SupabaseClientOptions = {};
+
+export const supabase: SupabaseClient = createClient(supabaseUrl, supabaseAnonKey, supaOptions);
